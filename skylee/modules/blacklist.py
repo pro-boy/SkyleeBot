b4b665d47d6a77598849d3487815ef82b4a87b2c
Crackexy <crackexy@gmail.com>
2020-11-10 20:08:38 +0530

    Update blacklist.py

--- skylee/modules/blacklist.py
+++ skylee/modules/blacklist.py
@@ -444,7 +444,7 @@ def __chat_settings__(chat_id, user_id):
 
 
 def __stats__():
-    return "✗ {} blacklist triggers, across {} chats.".format(
+    return "◉ {} blacklist triggers, across {} chats.".format(
         sql.num_blacklist_filters(), sql.num_blacklist_filter_chats()
     )
 
