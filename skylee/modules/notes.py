0a47e61e7524e8ceb2f721d6fc3f97f0971704e9
Crackexy <crackexy@gmail.com>
2020-11-10 15:03:13 +0530

    Update notes.py

--- skylee/modules/notes.py
+++ skylee/modules/notes.py
@@ -357,7 +357,7 @@ def list_notes(update, context):
             )
         except ValueError:
             update.effective_message.reply_text(
-                "There was a problem in showing notes list, maybe due to some invalid character in note names. Ask in @MeikoSupport if you're unable to figure it out!"
+                "There was a problem in showing notes list, maybe due to some invalid character in note names. Ask in @ChiyoSupport if you're unable to figure it out!"
             )
 
 
