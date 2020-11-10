3338f63c060e700e1e242085ab357b4a6668b5cd
Crackexy <crackexy@gmail.com>
2020-11-10 10:15:32 +0530

    Update backups.py

--- skylee/modules/backups.py
+++ skylee/modules/backups.py
@@ -328,7 +328,7 @@ def export_data(update, context):
         },
     }
     baccinfo = json.dumps(backup, indent=4)
-    f = open("SkyLee{}.backup".format(chat_id), "w")
+    f = open("Chiyo{}.backup".format(chat_id), "w")
     f.write(str(baccinfo))
     f.close()
     context.bot.sendChatAction(current_chat_id, "upload_document")
@@ -345,15 +345,15 @@ def export_data(update, context):
         pass
     context.bot.sendDocument(
         current_chat_id,
-        document=open("SkyLee{}.backup".format(chat_id), "rb"),
-        caption="*Successfully imported backup:*\nChat: `{}`\nChat ID: `{}`\nOn: `{}`\n\nNote: This `SkyLee-Backup` is specially made for notes.".format(
+        document=open("Chiyo{}.backup".format(chat_id), "rb"),
+        caption="*Successfully imported backup:*\nChat: `{}`\nChat ID: `{}`\nOn: `{}`\n\nNote: This `Chiyo-Backup` is specially made for notes.".format(
             chat.title, chat_id, tgl
         ),
         timeout=360,
         reply_to_message_id=msg.message_id,
         parse_mode=ParseMode.MARKDOWN,
     )
-    os.remove("SkyLee{}.backup".format(chat_id))  # Cleaning file
+    os.remove("Chiyo{}.backup".format(chat_id))  # Cleaning file
 
 
 # Temporary data
@@ -380,10 +380,10 @@ __mod_name__ = "Backups"
 __help__ = """
 *Only for chat administrator:*
 
- ✗ /import: Reply to the backup file for the butler / emilia group to import as much as possible, making transfers very easy! \
+ ◉ /import: Reply to the backup file for the butler / emilia group to import as much as possible, making transfers very easy! \
  Note that files / photos cannot be imported due to telegram restrictions.
 
- ✗ /export: Export group data, which will be exported are: rules, notes (documents, images, music, video, audio, voice, text, text buttons) \
+ ◉ /export: Export group data, which will be exported are: rules, notes (documents, images, music, video, audio, voice, text, text buttons) \
 
 """
 
